1b50a7542179b696893d35b087d4417f3dd0a1ef
David McKee <david.mckee@dxw.com>
2025-04-30 12:52:08 +0100

    Parse with named regex not partition

---
+++ release.py
@@ -0,0 +1,35 @@
+import re
+
+def parse_line(line):
+    regex = r"(?P<title>.*) by @(?P<author>.*) in (?=http)(?P<uri>.*)"
+    match = re.match(regex, line).groupdict()
+    return (match['title'], match['author'], match['uri'])
+
+with open("release.txt") as f:
+    release_lines=f.readlines()
+other=[]
+normal=[]
+renovate=[]
+for line in release_lines:
+    if line[:2] == "* ":
+        rest_of_line = line[2:]
+        title, author, uri = parse_line(rest_of_line)
+        new_line = f"* [{title}]({uri}) by @{author}"
+        if "@renovate" in line:
+            renovate.append(new_line)
+        else:
+            normal.append(new_line)
+    else:
+        other.append(line)
+for line in other:
+    print (line)
+for line in normal:
+    print (line)
+
+print ("#### Renovate")
+for line in renovate:
+    print (line)
+
+    
+    
+
